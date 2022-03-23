b1919988387ce0a9d8a65a8b216c93e9ec41066a
HuQiaoDong <534664357@qq.com>
2022-03-23 16:57:46 +0800

    webpack+vue3+sfc开发模板

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>asfasfsf</div>
+</template>
+
+<script lang="ts">
+import {defineComponent} from "vue";
+export default defineComponent({
+  name: "App"
+})
+</script>
+
+<style scoped>
+
+</style>
---
+++ webpack.config.js
@@ -0,0 +1,64 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { VueLoaderPlugin } = require('vue-loader')
+const path = require("path")
+console.log(__dirname)
+module.exports = {
+    entry: "./src/main.js",
+    output: {
+        filename: "bundle.js",
+        clean: true,
+        path: path.resolve(__dirname,"./dist")
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+    },
+    mode: "development",
+    devtool: "inline-source-map",
+    module: {
+        rules: [
+            // 混入.vue文件资源
+            {
+                test: /\.vue$/,
+                loader: 'vue-loader'
+            },
+            // 它会应用到普通的 `.js` 文件
+            // 以及 `.vue` 文件中的 `<script>` 块
+            {
+                test: /\.js$/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env'],
+                    }
+                },
+                exclude: path.resolve(__dirname,"./node_modules/")
+            },
+            // 它会应用到普通的 `.css` 文件
+            // 以及 `.vue` 文件中的 `<style>` 块
+            {
+                test: /\.css$/,
+                use: [
+                    'vue-style-loader',
+                    'css-loader'
+                ]
+            },
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: path.resolve(__dirname,"./node_modules/"),
+            },
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            inject: "body",
+            template: path.resolve(__dirname + "/public/index.html")
+        }),
+        new VueLoaderPlugin()
+    ],
+    devServer: {
+        static: "./dist",
+        port: 9001,
+        open: true
+    },
+}
