769efc3ed5ef669ed3af26f4189c1e14cce6ccde
HuQiaoDong <534664357@qq.com>
2022-03-23 17:53:54 +0800

    webpack+vue3+sfc 开发模板

--- webpack.config.js
+++ webpack.config.js
@@ -42,11 +42,6 @@ module.exports = {
                     'css-loader'
                 ]
             },
-            {
-                test: /\.tsx?$/,
-                use: 'ts-loader',
-                exclude: path.resolve(__dirname,"./node_modules/"),
-            },
         ]
     },
     plugins: [
